aa634ade71ad14815d119cc2026aeb4f2da69bda
NgawangT <ngawangjb22@gmail.com>
2023-10-10 00:53:53 +0100

    Main class updated with fruit class and shoppingBasket.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Fruit apple = new Fruit("APPLE", 0.33);
+        Fruit cherry = new Fruit("CHERRY", 0.10);
+        Fruit mango = new Fruit("MANGO", 0.45);
+
+
+        ShoppingBasket basket = new ShoppingBasket();
+
+        basket.addFruit(apple);
+        basket.addFruit(cherry);
+        basket.addFruit(mango);
+        basket.addFruit(apple);
+        basket.addFruit(cherry);
+        basket.addFruit(mango);
+
+        System.out.println(basket.getBasketItems());
+        System.out.println(basket.getFruitCount());
+        System.out.println(basket.calculateTotalCost());
+
     }
 }
